a7e223b253c1a46aec2d1682c939bde39094a913
whl <739753489@qq.com>
2019-05-16 11:54:44 +0800

    优惠券11.45

--- zycoupon/api.php
+++ zycoupon/api.php
@@ -14,6 +14,9 @@ class api
         $this->zycoupon_user_db = pc_base::load_model('zycoupon_user_model');
         $this->zyshoptype_db = pc_base::load_model('goodscat_model');
         $this->member_db = pc_base::load_model('member_model');
+
+        $this->_userid = param::get_cookie('_userid');
+
     }
 
     /**
@@ -23,7 +26,7 @@ class api
      */
     public function api_coupon_info($_userid)
     {
-        $_userid = empty($_GET['_userid']) ? $_POST['_userid'] : $_GET['_userid'];
+        $_userid = empty($_POST['_userid']) ? $this->_userid : $_POST['_userid'];
         $member_info = $this->member_db->get_one(array('userid'=>$_userid));
 
         if($member_info){
@@ -62,8 +65,8 @@ class api
      */
     public function api_coupon_take($_userid)
     {
-        $_userid = empty($_GET['_userid']) ? $_POST['_userid'] : $_GET['_userid'];
-        $_couponid = empty($_GET['_couponid']) ? $_POST['_couponid'] : $_GET['_couponid'];
+        $_userid = empty($_POST['_userid']) ? $this->_userid : $_POST['_userid'];
+        $_couponid = empty($_POST['_couponid']) ? $_POST['_couponid'] : $_POST['_couponid'];
 
         $member_info = $this->member_db->get_one(array('userid'=>$_userid));
         if($member_info){
@@ -107,8 +110,8 @@ class api
      */
     public function api_coupon_use($_userid)
     {
-        $_userid = empty($_GET['_userid']) ? $_POST['_userid'] : $_GET['_userid'];
-        $_coupon_user_id = empty($_GET['_coupon_user_id']) ? $_POST['_coupon_user_id'] : $_GET['_coupon_user_id'];
+        $_userid = empty($_POST['_userid']) ? $this->_userid : $_POST['_userid'];
+        $_coupon_user_id = empty($_POST['_coupon_user_id']) ? $_POST['_coupon_user_id'] : $_POST['_coupon_user_id'];
 
         $member_info = $this->member_db->get_one(array('userid'=>$_userid));
         if($member_info){
@@ -169,13 +172,84 @@ class api
     }
 
     /**
+     * 选择优惠券
+     * @param $_userid 用户ID
+     * @param _coupon_user_id 优惠券用户关联表ID
+     * @return json
+     * @internal param 关联表id $_coupon_user_id
+     */
+    public function api_coupon_select($_userid)
+    {
+        $_userid = empty($_POST['_userid']) ? $this->_userid : $_POST['_userid'];
+        $_coupon_user_id = empty($_POST['_coupon_user_id']) ? $_GET['_coupon_user_id'] : $_POST['_coupon_user_id'];
+
+        $member_info = $this->member_db->get_one(array('userid'=>$_userid));
+        if($member_info){
+            if($_coupon_user_id!=0) {
+                $coupon_user_info = $this->zycoupon_user_db->get_one("id=" . $_coupon_user_id . " AND isused=0 and userid=" . $_userid);
+                if ($coupon_user_info) {
+                    $coupon_info = $this->zycoupon_db->get_one("id=" . $coupon_user_info['coupon'] . " AND status=1 AND takenum-usednum>0");
+                    if ($coupon_info['vaild_type'] == 1) {
+                        if ($coupon_info['begintime'] <= time() && $coupon_info['endtime'] >= time()) {
+                            //业务流程
+                            $data['isselect'] = 0;
+                            $info = $this->zycoupon_user_db->update($data, 'userid=' . $_userid);
+
+                            $data['isselect'] = 1;
+                            $info1 = $this->zycoupon_user_db->update($data, 'id=' . $_coupon_user_id);
+
+                        } else {
+                            returnjsoninfo('-3', '优惠券已过期');
+                        }
+                    } else if ($coupon_info['vaild_type'] == 2) {
+                        if (($coupon_info['days'] * 3600 * 24) + $coupon_user_info['gettime'] >= time()) {
+                            //业务流程
+                            $data['isselect'] = 0;
+                            $info = $this->zycoupon_user_db->update($data, 'userid=' . $_userid);
+
+                            $data['isselect'] = 1;
+                            $info1 = $this->zycoupon_user_db->update($data, 'id=' . $_coupon_user_id);
+                        } else {
+                            returnjsoninfo('-5', '优惠券已过期');
+                        }
+                    } else {
+                        returnjsoninfo('-4', '优惠券无效');
+                    }
+                } else {
+                    returnjsoninfo('-2', '优惠券不存在');
+                }
+            }else{
+                $data['isselect'] = 0;
+                $info = $this->zycoupon_user_db->update($data, 'userid=' . $_userid);
+                returnAjaxData('200','取消选择成功',$info);
+            }
+        }else{
+            returnjsoninfo('-1','用户为空');
+        }
+
+        if($info&&$info1){
+            //returnjsoninfo('200','操作成功',$info);
+            $json['status']='success';
+            $json['code']='200';
+            $json['message']='选择成功';
+            $json['data']=$info;
+        }else{
+            //returnjsoninfo('-200','数据为空');
+            $json['status']='error';
+            $json['code']='-200';
+            $json['message']='选择失败';
+        }
+        exit(json_encode($json,JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT));
+    }
+
+    /**
      * 获取用户所有可用的优惠券
      * @param $_userid
      * @return json
      */
     public function api_coupon_user($_userid)
     {
-        $_userid = empty($_GET['_userid']) ? $_POST['_userid'] : $_GET['_userid'];
+        $_userid = empty($_POST['_userid']) ? $this->_userid : $_POST['_userid'];
         $member_info = $this->member_db->get_one(array('userid'=>$_userid));
 
         if($member_info){
@@ -249,9 +323,9 @@ class api
      * @internal param 关联表id $_coupon_user_id
      */
     public function order_coupon_count(){
-        $_userid = empty($_GET['_userid']) ? $_POST['_userid'] : $_GET['_userid'];
-        $_catid = empty($_GET['_catid']) ? $_POST['_catid'] : $_GET['_catid'];
-        $_total = empty($_GET['_total']) ? $_POST['_total'] : $_GET['_total'];
+        $_userid = empty($_POST['_userid']) ? $this->_userid : $_POST['_userid'];
+        $_catid = empty($_POST['_catid']) ? $_POST['_catid'] : $_POST['_catid'];
+        $_total = empty($_POST['_total']) ? $_POST['_total'] : $_POST['_total'];
         $total=0;
         foreach($_total as $value) {
             $total+=$value;
@@ -259,34 +333,44 @@ class api
 
         $member_info = $this->member_db->get_one(array('userid'=>$_userid));
         if($member_info) {
-
-            $where='isused=0 AND isselect=0 AND userid='.$_userid;
-            $where.=' AND ((vaild_type=2 AND UNIX_TIMESTAMP(NOW())<(gettime+days*24*3600)) OR (vaild_type=1 AND begintime<UNIX_TIMESTAMP(NOW()) AND endtime>UNIX_TIMESTAMP(NOW()) AND `status`=1)) ';
-            if($_catid){
-                $w='';
-                foreach($_catid as $k=> $value){
-                    $w.=' OR  limittype='.$_catid[$k];
-                }
-                $where.=' AND (limittype=0 '.$w.')';
-            }
-            if($_total){
-                $w='';
-                foreach($_catid as $k=> $value){
-                    $w.=' OR (type!=0  AND limittype='.$_catid[$k].' AND full<'.$_total[$k]. ')';
-                }
-                $where.=' AND (type=0 '.$w.')';
-            }
-            $sql='SELECT count(*) AS num FROM zy_zycoupon_user u LEFT JOIN zy_zycoupon c ON u.coupon =c.id WHERE '.$where;
-            $info = $this->zycoupon_user_db->spcSql($sql,1,1);
-            if($info) {
+            //(array('userid'=>$_userid,'isselect'=>1))
+            $where=' userid='.$_userid.' AND u.isselect=1';
+            $sql='SELECT *  FROM zy_zycoupon c  JOIN zy_zycoupon_user u ON u.coupon =c.id WHERE ' . $where;
+            $select_info = $this->zycoupon_user_db->spcSql($sql, 1, 1);
+            if($select_info) {
                 $json['status'] = 'success';
-                $json['code'] = '200';
-                $json['message'] = '有可用优惠券';
-                $json['data'] = $info;
+                $json['code'] = '100';
+                $json['message'] = '已选择优惠券';
+                $json['data'] = $select_info;
             }else{
-                $json['status'] = 'error';
-                $json['code'] = '-200';
-                $json['message'] = '无可用优惠券';
+                $where = 'isused=0 AND isselect=0 AND userid=' . $_userid;
+                $where .= ' AND ((vaild_type=2 AND UNIX_TIMESTAMP(NOW())<(gettime+days*24*3600)) OR (vaild_type=1 AND begintime<UNIX_TIMESTAMP(NOW()) AND endtime>UNIX_TIMESTAMP(NOW()) AND `status`=1)) ';
+                if ($_catid) {
+                    $w = '';
+                    foreach ($_catid as $k => $value) {
+                        $w .= ' OR  limittype=' . $_catid[$k];
+                    }
+                    $where .= ' AND (limittype=0 ' . $w . ')';
+                }
+                if ($_total) {
+                    $w = '';
+                    foreach ($_catid as $k => $value) {
+                        $w .= ' OR (type!=0  AND limittype=' . $_catid[$k] . ' AND full<' . $_total[$k] . ')';
+                    }
+                    $where .= ' AND (type=0 ' . $w . ')';
+                }
+                $sql = 'SELECT count(*) AS num FROM zy_zycoupon_user u LEFT JOIN zy_zycoupon c ON u.coupon =c.id WHERE ' . $where;
+                $info = $this->zycoupon_user_db->spcSql($sql, 1, 1);
+                if ($info) {
+                    $json['status'] = 'success';
+                    $json['code'] = '200';
+                    $json['message'] = '有可用优惠券';
+                    $json['data'] = $info;
+                } else {
+                    $json['status'] = 'error';
+                    $json['code'] = '-200';
+                    $json['message'] = '无可用优惠券';
+                }
             }
         }else{
             $json['status'] = 'error';
@@ -303,17 +387,21 @@ class api
      * @internal param 关联表id $_coupon_user_id
      */
     public function order_coupon_info(){
-        $_userid = empty($_GET['_userid']) ? $_POST['_userid'] : $_GET['_userid'];
-        $_catid = empty($_GET['_catid']) ? $_POST['_catid'] : $_GET['_catid'];
-        $_total = empty($_GET['_total']) ? $_POST['_total'] : $_GET['_total'];
+        $_userid = empty($_POST['_userid']) ? $this->_userid : $_POST['_userid'];
+        $_catid = empty($_POST['_catid']) ? $_POST['_catid'] : $_POST['_catid'];
+        $_total = empty($_POST['_total']) ? $_POST['_total'] : $_POST['_total'];
 
         $_catid=explode(',',$_catid);
         $_total=explode(',',$_total);
 
+        if(empty($_catid)||empty($_total)){
+            returnjsoninfo('-2','参数不完整');
+        }
+
         $member_info = $this->member_db->get_one(array('userid'=>$_userid));
         if($member_info) {
 
-            $where='isused=0 AND isselect=0 AND userid='.$_userid;
+            $where='isused=0 AND userid='.$_userid;
             $where.=' AND ((vaild_type=2 AND UNIX_TIMESTAMP(NOW())<(gettime+days*24*3600)) OR (vaild_type=1 AND begintime<UNIX_TIMESTAMP(NOW()) AND endtime>UNIX_TIMESTAMP(NOW()) AND `status`=1)) ';
             if($_catid){
                 $w='';
